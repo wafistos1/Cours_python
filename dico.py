6f42484084cdf9939ee4f922eb6937805b9d9a2a
wafistos <mameri.wafi@gmail.com>
2019-03-20 11:20:45 +0100

    salut

---
+++ dico.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# conding: utf-8
+
+
+"""
+    Cours de Jason de la formation video
+"""
+echiquier = {}# Creation d'un dictionnaire
+echiquier['a', 1] = "tour blanche"
+echiquier['b', 1] = "cavalier blanc"
+echiquier['c', 1] = "fou blanc"
+echiquier['d', 1] = "reine blanche"
+
+
+# Premiere ligne
+echiquier['a', 2] = "pion blanc"
+echiquier['b', 2] = "pion blanc"
+
+
+print(echiquier)
+
+if __name__ == "__main__":
+    print("Salut")
